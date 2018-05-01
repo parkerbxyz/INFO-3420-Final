0d032cb7097c79670a6ef6aa40dcac7da6ee1d14
Parker Brown <17183625+parkerbxyz@users.noreply.github.com>
2018-04-30 23:27:13 -0600

    Final WIP

---
+++ INFO-3420-Final/Models/Donation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace INFO_3420_Final.Models
+{
+    public class Donation
+    {
+        [Required]
+        [Display(Name = "Donation Date")]
+        public DateTime DonationDate { get; set; }
+
+        public string Username { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(100, MinimumLength = 2)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(100, MinimumLength = 2)]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        [Required]
+        [Display(Name = "Postal Code")]
+        [DataType(DataType.PostalCode)]
+        public string PostalCode { get; set; }
+
+        [Required]
+        [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+
+        [Required]
+        [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Range(0.00, 9999.99)]
+        public decimal Total { get; set; }
+    }
+}
---
+++ INFO-3420-Final/Models/Location.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace INFO_3420_Final.Models
+{
+    public class Location
+    {
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+
+        public Location(string address, string city, string state, int zip)
+        {
+            Address = address;
+            City = city;
+            State = state;
+            Zip = zip;
+        }
+    }
+}
